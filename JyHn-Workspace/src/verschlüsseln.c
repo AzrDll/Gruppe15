c9d4d81fe198f5463c9cc0eb86de56c72371596b
Jenny04H <j.herrmann@danner-it.de>
2024-02-12 14:53:21 +0100

    Verschlüsseln.c

--- JyHn-Workspace/src/verschlüsseln.c
+++ JyHn-Workspace/src/verschlüsseln.c
@@ -4,33 +4,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+typedef struct 
+{
     unsigned char r;
     unsigned char g;
     unsigned char b;
 } RGBPixel;
 
 
-int main(int argc, char** argv){
+int main(int argc, char** argv)
+{
  int width, height, channels;
 
     unsigned char *img = stbi_load("./Eifelturm-767x522.jpg", &width, &height, &channels, 0);
-    if(img == NULL){
+    if(img == NULL)
+    {
         printf("Error: Bild konnte nicht geladen werden\n");
         exit (1);
     }
     printf("Success: Bild wurde geladen!\n");
 
     RGBPixel* rgbArray = (RGBPixel*)malloc((width) * (height) * sizeof(RGBPixel));
-    if(rgbArray == NULL) {
+    if(rgbArray == NULL) 
+    {
         printf("Error: Speicher konnte nicht allokiert werden\n");
         exit(1);
     }
 
     // Extrahiere Pixel in RGB die als Binary gespeichert sind
     int pixelIndex = 0;
-    for(int y = 0; y < height; y++) {
-        for(int x = 0; x < width; x++) {
+    for(int y = 0; y < height; y++) 
+    {
+        for(int x = 0; x < width; x++) 
+        {
             unsigned char *p = img + (y * (width) + x) * (channels);
             rgbArray[pixelIndex].r = p[0];
             rgbArray[pixelIndex].g = p[1];
@@ -43,15 +49,18 @@ int main(int argc, char** argv){
     char* secret = "Hello :)";
     size_t secret_len = strlen(secret);
 
-    for (size_t i = 0; i < secret_len; i++) {
+    for (size_t i = 0; i < secret_len; i++) 
+    {
         rgbArray[pixelIndex].r = secret[i];
         pixelIndex++;
     }
 
     // Speichere das Geheimnis zurück in das Bild
     pixelIndex = 0;
-    for(int y = 0; y < height; y++) {
-        for(int x = 0; x < width; x++) {
+    for(int y = 0; y < height; y++) 
+    {
+        for(int x = 0; x < width; x++) 
+        {
             unsigned char *p = img + (y * (width) + x) * (channels);
             p[0] = rgbArray[pixelIndex].r;
             p[1] = rgbArray[pixelIndex].g;
@@ -61,7 +70,7 @@ int main(int argc, char** argv){
     }
 
     // Speichere das Bild als Datei ab
-    stbi_write_jpg("out.jpg", width, height, channels, img, 100);
+    stbi_write_pmg("out.pmg", width, height, channels, img, 100);
 
     // Freigabe des Speicherplatzes
     free(rgbArray);
